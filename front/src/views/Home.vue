f3e0c74808f276f81f72c32f422ad3630076e7d3
Erkin <esultonovv@gmail.com>
2021-03-27 23:40:18 +0500

    design changes

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <v-container class="d-flex justify-center align-center flex-column">
-      <v-btn class="ma-3" color="success" to="/join">Join to quiz</v-btn>
+  <div class="main-section">
+    <v-container>
+      <v-btn class="ma-3" color="success" to="/join">Join quiz</v-btn>
       <v-btn color="warning">Create quiz</v-btn>
     </v-container>
   </div>
@@ -12,3 +12,22 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style>
+.v-main__wrap{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+.main-section .container .v-btn{
+  width: 300px;
+  height: 300px !important;
+  border-radius: 50%;
+}
+.main-section .container .v-btn span{
+  font-size: 20px;
+}
+.main-section .container .v-btn:first-child{
+  margin-right: 50px !important;
+}
+</style>

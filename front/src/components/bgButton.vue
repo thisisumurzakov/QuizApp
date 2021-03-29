15571237b8785ffa5b24742cd4005aacb841dedd
Abdulaziz <abdulazizmannopov07@gmail.com>
2021-03-30 02:13:00 +0500

    fuckin animations

---
+++ front/src/components/bgButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-col
+      md="6"
+      sm="12"
+      class="justify-center d-flex"
+  >
+    <v-btn
+        class="bg-btn display-1 rounded-circle btn-transition"
+        height="300px"
+        width="300px"
+        :color="color"
+        :to="address"
+    >
+      <span class="button-icon">
+        <v-icon
+            x-large
+            color="white"
+        >
+          {{ innerIcon }}
+        </v-icon>
+      </span>
+      <span class="button-text">
+        {{ title }}
+      </span>
+    </v-btn>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: ['color', 'title', 'innerIcon', 'address'],
+  name: "bgButton"
+}
+</script>
+
+<style scoped>
+.btn-transition {
+}
+.button-text {
+  opacity: 0;
+}
+.button-icon {
+  margin-bottom: 10px;
+}
+.btn-transition:hover .button-text {
+  opacity: 1;
+}
+.btn-transition:hover .button-icon {
+  opacity: 0;
+}
+.button-icon, .button-text {
+  position: absolute;
+  transition: .3s all;
+  transform-origin: center;
+}
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,64 +1,55 @@
 <template>
   <div class="d-flex align-center" style="height: 100%; width: 100%;">
-    <v-container class="justify-center d-flex">
-      <v-btn
-          class="bg-btn mr-3 title rounded-circle custom-class"
-          height="300px"
-          width="300px"
-          color="success"
-          to="/join"
-      >
-        <span class="button-text">Join quiz</span>
-        <span class="button-icon"><v-icon
-
-            color="white"
-        >
-      mdi-play
-    </v-icon></span>
-      </v-btn>
-      <v-btn
-          class="bg-btn ml-3 title rounded-circle custom-class"
-          height="300px"
-          width="300px"
-          color="warning"
-      >
-        <span class="button-text">Create quiz</span>
-        <span class="button-icon"> <v-icon
-
-            color="white"
-        >
-      mdi-plus
-    </v-icon></span>
-      </v-btn>
+    <v-container>
+      <v-row align-content="center">
+        <bg-button
+            title="Create quiz"
+            color="warning"
+            inner-icon="mdi-plus"
+            address=""
+        />
+        <bg-button
+            title="Join to quiz"
+            color="success"
+            inner-icon="mdi-play"
+            address="/join"
+        />
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
+import bgButton from '@/components/bgButton';
+
 export default {
+  components: {
+    bgButton
+  },
   name: 'Home'
 }
 </script>
 
 <style scoped>
 
-.custom-class{
+.custom-class {
   transition: 0.5s;
-
 }
-.custom-class:hover{
+
+.custom-class:hover {
   transform: rotate(360deg);
 }
 
-.custom-class:hover .button-icon{
+.custom-class:hover .button-icon {
   display: block;
 }
 
-.custom-class:hover .button-text{
+
+.custom-class:hover .button-text {
   display: none;
 }
 
-.custom-class .button-icon{
+.custom-class .button-icon {
   display: none;
 }
 
@@ -68,12 +59,12 @@ export default {
 }
 
 
-.custom-class span{
+.custom-class span {
   font-size: 20px !important;
   transition: 0.5s;
 }
 
-.custom-class span{
+.custom-class span {
   font-size: 30px !important;
 }
 </style>

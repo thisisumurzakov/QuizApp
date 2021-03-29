35b8b2a018b8610cac53a6a8400b25bab15b020b
Erkin <esultonovv@gmail.com>
2021-03-30 00:23:15 +0500

    animation

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,21 +2,33 @@
   <div class="d-flex align-center" style="height: 100%; width: 100%;">
     <v-container class="justify-center d-flex">
       <v-btn
-          class="bg-btn mr-3 title rounded-circle"
+          class="bg-btn mr-3 title rounded-circle custom-class"
           height="300px"
           width="300px"
           color="success"
           to="/join"
       >
-        Join quiz
+        <span class="button-text">Join quiz</span>
+        <span class="button-icon"><v-icon
+
+            color="white"
+        >
+      mdi-play
+    </v-icon></span>
       </v-btn>
       <v-btn
-          class="bg-btn ml-3 title rounded-circle"
+          class="bg-btn ml-3 title rounded-circle custom-class"
           height="300px"
           width="300px"
           color="warning"
       >
-        Create quiz
+        <span class="button-text">Create quiz</span>
+        <span class="button-icon"> <v-icon
+
+            color="white"
+        >
+      mdi-plus
+    </v-icon></span>
       </v-btn>
     </v-container>
   </div>
@@ -29,4 +41,39 @@ export default {
 </script>
 
 <style scoped>
+
+.custom-class{
+  transition: 0.5s;
+
+}
+.custom-class:hover{
+  transform: rotate(360deg);
+}
+
+.custom-class:hover .button-icon{
+  display: block;
+}
+
+.custom-class:hover .button-text{
+  display: none;
+}
+
+.custom-class .button-icon{
+  display: none;
+}
+
+.custom-class .button-icon i {
+  color: #fff;
+  font-size: 150px;
+}
+
+
+.custom-class span{
+  font-size: 20px !important;
+  transition: 0.5s;
+}
+
+.custom-class span{
+  font-size: 30px !important;
+}
 </style>

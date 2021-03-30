6050b3cb5a9c97202aa7c69acb3672efaf1d067e
Abdulaziz <abdulazizmannopov07@gmail.com>
2021-03-30 20:46:46 +0500

    added carousel background

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="d-flex align-center" style="height: 100%; width: 100%;">
-    <v-container>
-      <v-row align-content="center">
-        <bg-button
-            title="Create quiz"
-            color="warning"
-            inner-icon="mdi-plus"
-            address="/blet"
-        />
-        <bg-button
-            title="Join to quiz"
-            color="success"
-            inner-icon="mdi-play"
-            address="/join"
-        />
-      </v-row>
-    </v-container>
+  <div style="height: 100%; position: relative;">
+    <v-carousel
+        :show-arrows="false"
+        cycle
+        interval="3000"
+        hide-delimiters
+        style="height: 100%;"
+    >
+      <v-carousel-item
+          v-for="color in colors"
+          :key="color"
+          :style="{
+            height: '100%',
+            background: color + ' no-repeat fixed center',
+            backgroundSize:'cover'}"
+      >
+
+      </v-carousel-item>
+    </v-carousel>
+    <div
+        class="d-flex align-center"
+        style="height: 100%; width: 100%; position: absolute;top: 0"
+    >
+      <v-container>
+        <v-row align-content="center">
+          <bg-button
+              title="Create quiz"
+              color="warning"
+              inner-icon="mdi-plus"
+              address="/blet"
+          />
+          <bg-button
+              title="Join to quiz"
+              color="success"
+              inner-icon="mdi-play"
+              address="/join"
+          />
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -23,6 +46,15 @@
 import bgButton from '@/components/bgButton';
 
 export default {
+  data: () => ({
+    model: 0,
+    colors: [
+      'url("https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg")',
+      'url("https://cdn.vuetifyjs.com/images/carousel/sky.jpg")',
+      'url("https://cdn.vuetifyjs.com/images/carousel/bird.jpg")',
+      'url("https://cdn.vuetifyjs.com/images/carousel/planet.jpg")'
+    ],
+  }),
   components: {
     bgButton
   },

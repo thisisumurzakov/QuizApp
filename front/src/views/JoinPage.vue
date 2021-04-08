b1ef03f4843754f4002c8aca9537ef1c036dd9dd
Abdulaziz <abdulazizmannopov07@gmail.com>
2021-04-08 15:37:37 +0500

    added validation and removed field for name

--- front/src/views/JoinPage.vue
+++ front/src/views/JoinPage.vue
@@ -14,20 +14,15 @@
         <v-card-actions>
           <v-text-field
             outlined
-            color="success"
+            success
+            :rules="rules"
             label="Room ID"
             class="mr-2"
             v-model="roomID"
           />
-          <v-text-field
-            outlined
-            color="warning"
-            label="Your name"
-            v-model="name"
-          />
         </v-card-actions>
         <v-card-actions class="d-flex justify-end" style="width: 100%">
-          <v-btn @click="join">Join</v-btn>
+          <v-btn @click="join" :disabled="!validation">Join</v-btn>
         </v-card-actions>
       </v-card>
     </v-container>
@@ -39,12 +34,17 @@ export default {
   name: "JoinPage",
   data: () => ({
     roomID: '',
-    name: ''
+    rules: [val => (val || '').length > 10 || 'This field is required']
   }),
   methods: {
     async join() {
-      await this.$store.dispatch('join', {roomID: this.roomID, name: this.name})
+      await this.$store.dispatch('join', {roomID: this.roomID})
       await this.$router.push(`/quiz/${this.roomID}/home`)
+    }
+  },
+  computed: {
+    validation() {
+      return this.roomID.length >= 10
     }
   }
 }
